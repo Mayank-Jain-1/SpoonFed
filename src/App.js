bfb569e62ede96552434c67e9c1f9259d8cb49a0
Mayank <kaushalmayank.k@gmail.com>
2022-12-18 21:07:43 +0530

    Responsive header complete

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import bgimage from "./Images/bg.png";
 function App() {
   return (
     <>
-      <div className="cover container-fluid d-flex flex-column justify-content-center align-items-center p-5">
+      <div className="cover container-fluid d-flex flex-column justify-content-center align-items-center p-3">
         <img src={bgimage} alt="" className="img-fluid bg-img" />
-        <h1 className="text-center">h2</h1>
-        <h3 className="text-center">Find the best restaurants, cafés, and bars</h3>
-        <div className="row w-100 justify-content-center gx-5 gy-2">
-          <input placeholder="Please type a location" value={""} className="col-lg-2 col-md-3 bg-white text-dark border py-3 mx-3  cl-dg"/>
-          <div className=" d-flex align-items-center col-lg-4 col-md-6 bg-white text-dark border px-0 mx-2 cl-dg position-relative">
+        <h1 id="logo" className="text-center text-danger bg-white p-4 rounded-circle mb-4">Sf!</h1>
+        <h2 className="title text-center mb-4  p1">Find the best restaurants, cafés, and bars</h2>
+        <div className="row w-100 justify-content-center px-4 gx-5 gy-4">
+          <select id="location" name="location" className="col-xl-2 col-lg-3 col-md-3 bg-white text-dark border py-3 mx-3  cl-db">
+          <option value="none">Select a location</option>
+          <option value="Ludhiana">Ludhiana</option>
+          <option value="Chandigarh">Chandigarh</option>
+          <option value="Mumbai">Mumbai</option>
+          <option value="Chennai">Chennai</option>
+          <option value="Dehradun">Dehradun</option>
+          <option value="Delhi">Delhi</option>
+          </select>
+          <div className=" d-flex col-xl-4 align-items-center col-lg-5 col-md-6 bg-white text-dark border px-0 mx-2 cl-dg position-relative">
           <i id="searchIcon" className="zmdi zmdi-search zmdi-hc-lg mx-1"></i>
-            <input id='inpLocation' type="text" className="h-100 w-100 px-5" placeholder="Hello"/>
+            <input id='inpLocation' type="text" className="h-100 w-100 px-5 cl-db" placeholder="Search for restaurants"/>
           </div>
         </div>
       </div>
