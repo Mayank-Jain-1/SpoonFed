54578bc24be973020cefb3c09b643a2ef94cb776
Mayank <kaushalmayank.k@gmail.com>
2022-12-18 20:22:52 +0530

    Icon link added (ZMDI) and input box added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
         <img src={bgimage} alt="" className="img-fluid bg-img" />
         <h1 className="text-center">h2</h1>
         <h3 className="text-center">Find the best restaurants, cafés, and bars</h3>
-        <div className="row w-100 justify-content-center gx-5">
+        <div className="row w-100 justify-content-center gx-5 gy-2">
           <input placeholder="Please type a location" value={""} className="col-lg-2 col-md-3 bg-white text-dark border py-3 mx-3  cl-dg"/>
-          <div className="col-lg-4 col-md-6 bg-white text-dark border py-3 mx-2  cl-dg">
-            <i ></i>
-            <input type="text" className="d-flex w-100"/>
+          <div className=" d-flex align-items-center col-lg-4 col-md-6 bg-white text-dark border px-0 mx-2 cl-dg position-relative">
+          <i id="searchIcon" className="zmdi zmdi-search zmdi-hc-lg mx-1"></i>
+            <input id='inpLocation' type="text" className="h-100 w-100 px-5" placeholder="Hello"/>
           </div>
         </div>
       </div>
