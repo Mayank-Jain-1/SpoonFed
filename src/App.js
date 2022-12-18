52b349f12ebd2f4fd4ded66d6fba9412de2fa7ee
Mayank <kaushalmayank.k@gmail.com>
2022-12-18 19:48:19 +0530

    Added Image and input boxe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import bgimage from "./Images/bg.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="cover container-fluid d-flex flex-column justify-content-center align-items-center p-5">
+        <img src={bgimage} alt="" className="img-fluid bg-img" />
+        <h1 className="text-center">h2</h1>
+        <h3 className="text-center">Find the best restaurants, cafés, and bars</h3>
+        <div className="row w-100 justify-content-center gx-5">
+          <input placeholder="Please type a location" value={""} className="col-lg-2 col-md-3 bg-white text-dark border py-3 mx-3  cl-dg"/>
+          <div className="col-lg-4 col-md-6 bg-white text-dark border py-3 mx-2  cl-dg">
+            <i ></i>
+            <input type="text" className="d-flex w-100"/>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
