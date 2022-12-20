4cd8b0d294b11dbcf91f2a5a04edfaf9b5f5ee46
Mayank <kaushalmayank.k@gmail.com>
2022-12-21 02:29:54 +0530

    Updates

---
+++ precache-manifest.7c322eb71001028713858e3af5e2dcac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1adf8efa22a5121759a2",
+    "url": "/spoon-fed.github.io/static/css/main.a868c6fc.chunk.css"
+  },
+  {
+    "revision": "1adf8efa22a5121759a2",
+    "url": "/spoon-fed.github.io/static/js/main.1adf8efa.chunk.js"
+  },
+  {
+    "revision": "69e0dab03ec9f6c99eb0",
+    "url": "/spoon-fed.github.io/static/js/1.69e0dab0.chunk.js"
+  },
+  {
+    "revision": "e9f230c634762e94b081",
+    "url": "/spoon-fed.github.io/static/css/2.57735097.chunk.css"
+  },
+  {
+    "revision": "e9f230c634762e94b081",
+    "url": "/spoon-fed.github.io/static/js/2.e9f230c6.chunk.js"
+  },
+  {
+    "revision": "c362a68487e67d485179",
+    "url": "/spoon-fed.github.io/static/js/runtime~main.c362a684.js"
+  },
+  {
+    "revision": "a86a8676d25e330e09ef4a763ae78536",
+    "url": "/spoon-fed.github.io/static/media/bg.a86a8676.png"
+  },
+  {
+    "revision": "17d9005d2075261c5930a7cedb71449b",
+    "url": "/spoon-fed.github.io/index.html"
+  }
+];
