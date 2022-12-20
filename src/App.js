72da86adf569da103088f0211d7b8930c06843c2
Mayank <kaushalmayank.k@gmail.com>
2022-12-20 23:08:19 +0530

    Responsiveness set for input boxes and header navbar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
         <h1 id="logo" className="text-center text-danger bg-white p-4 rounded-circle mb-4">Sf!</h1>
         <h2 className="title text-center mb-4  p1">Find the best restaurants, cafés, and bars</h2>
         <div className="row w-100 justify-content-center px-4 gx-5 gy-4">
-          <select id="location" name="location" className="col-xl-2 col-lg-3 col-md-3 bg-white text-dark border px-3 py-3 mx-3 cl-db">
+          <select id="location" name="location" className="col-xl-2 col-lg-3 col-md-3 col-sm-9 col-xs-12
+          bg-white text-dark border px-3 py-3 mx-3 cl-db">
           <option value="none">Select a location</option>
           <option value="Ludhiana">Ludhiana</option>
           <option value="Chandigarh">Chandigarh</option>
@@ -20,7 +21,7 @@ function App() {
           <option value="Dehradun">Dehradun</option>
           <option value="Delhi">Delhi</option>
           </select>
-          <div className=" d-flex col-xl-4 align-items-center col-lg-5 col-md-6 bg-white text-dark border px-0 mx-2 cl-dg position-relative">
+          <div className=" d-flex col-xl-4 align-items-center col-lg-5 col-md-6 col-sm-9 col-xs-12 bg-white text-dark border px-0 mx-2 cl-dg position-relative">
           <i id="searchIcon" className="zmdi zmdi-search zmdi-hc-lg mx-1"></i>
             <input id='inpLocation' type="text" className="h-100 w-100 px-5 cl-db" placeholder="Search for restaurants"/>
           </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <nav className='row w-100 position-absolute d-flex justify-content-end px-5 mt-3'>
-      <a className='col-1 p-3 text-decoration-none text-white mx-4' href="">Login</a>
-      <a className='col-2 p-3 text-decoration-none text-white border border-white border-2 navlink' href="">Create an account</a>
+    <nav className='row w-100 position-absolute d-none d-md-flex justify-content-end px-5 mt-3 '>
+      <a className='col-1 p-3 text-decoration-none text-white mx-4' href="https://google.com">Login</a>
+      <a className='col-2 p-3 text-decoration-none text-white border border-white border-2 navlink' href="https://google.com">Create an account</a>
     </nav>
   )
 }
