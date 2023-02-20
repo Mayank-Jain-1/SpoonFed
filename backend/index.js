32c2bb00e59b1369b126f366818ecfa54d5f9082
Mayank <kaushalmayank.k@gmail.com>
2023-02-21 01:04:28 +0530

    updated the data. Changed New Delhi to Delhi

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,7 @@ connectDB();
 
 const Restaurants = require("./models/restaurants.js");
 
-
 const filter = require("./controllers/filter");
-
 
 app.listen(3000);
 app.use(express.json());
@@ -16,6 +14,19 @@ app.use((req, res, next) => {
   console.log(req.method + ":" + req.url);
   next();
 });
+
+const funct =  (req,res) => {
+  Restaurants.find({city_name: "Delhi" }, (err, restaurants) => {
+    if (err) return console.log(err);
+    res.send({length: restaurants.length, restaurants: restaurants})
+;
+  });
+  // Restaurants.updateMany({city_name: "New Delhi"}, {$set: {city_name: "Delhi"}} ,(err, result) => {
+  //   if(err) return res.send(err);
+  //   res.send(result);
+  // })
+};
+
 
 app.get("/restaurants", async (req, res) => {
   Restaurants.find((err, data) => {
@@ -26,7 +37,9 @@ app.get("/restaurants", async (req, res) => {
   });
 });
 
-app.get("/restaurants/filter",  filter);
+app.get("/restaurants/filter", filter);
+
+app.get("/find", funct);
 
 //   'Mumbai',
 //   'Delhi',
