53f77329dd6755c8d0fc534db9051daebe8dd16c
Mayank <kaushalmayank.k@gmail.com>
2023-03-11 03:13:05 +0530

    Made the filter results dynamic and Links to the restaurant page

--- client/src/components/Filter/FilterResult.jsx
+++ client/src/components/Filter/FilterResult.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+//TODO : inspite of taking full restaurant only take the required information as props.
 
-const FilterResult = ({restaurant}) => {
+const FilterResult = ({ restaurant }) => {
+  const cuisines = restaurant.Cuisine
+    ? restaurant.Cuisine.map((cuisine) => cuisine.name)
+    : [];
+
   return (
-    <div className="product shadow-md bg-white mb-36 w-100 px-35 py-25">
-    <div className="productHead d-flex pb-30 border-2 border-bottom align-items-center">
-      
-      <img src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className='object-cover rounded-4 square-120 me-3 flex-shrink-0'/>
-      <div className="placeDesc ms-4">
-        <h2 className="text-primary fw-bold">{restaurant &&  restaurant.name}</h2>
-        <h6 className="text-primary">Fort</h6>
-        <p className="text-primary mb-0 fw-light">
-          Shop 1, Plot D, Samruddhi Complex, Chincholi …
-        </p>
+    <Link to={"/restaurant/" + restaurant._id} className="d-block text-decoration-none product shadow-md bg-white mb-36 w-100 px-35 py-25">
+      <div className="productHead d-flex pb-30 border-2 border-bottom align-items-center">
+        <img
+          src={restaurant.thumb}
+          alt=""
+          className="object-cover rounded-4 square-120 me-3 flex-shrink-0"
+        />
+        <div className="placeDesc ms-4">
+          <h2 className="text-primary fw-bold">{restaurant.name}</h2>
+          <h6 className="text-primary">{restaurant.locality}</h6>
+          <p className="text-primary mb-0 fw-light">{restaurant.address}</p>
+        </div>
       </div>
-    </div>
 
-    <div className="productFoot mt-4 d-flex">
-      <div className="featureLabels text-secondary me-2 pe-1">
-        <p className='mb-2'>CUISINES:</p>
-        <p className='mb-0'>COST FOR TWO:</p>
+      <div className="productFoot mt-4 d-flex">
+        <div className="featureLabels text-secondary me-2 pe-1">
+          <p className="mb-2">CUISINES:</p>
+          <p className="mb-0">COST FOR TWO:</p>
+        </div>
+        <div className="features text-primary ms-4">
+          <p className="mb-2">{cuisines.join(", ")}</p>
+          <p className="mb-0">
+            ₹
+            {` ${restaurant.cost - (restaurant.cost % 100)} - ${
+              restaurant.cost - (restaurant.cost % 100) + 100
+            }`}
+          </p>
+        </div>
       </div>
-      <div className="features text-primary ms-4">
-        <p className='mb-2'>Bakery</p>
-        <p className='mb-0'>₹700</p>
-      </div>
-    </div>
-  </div>
-  )
-}
+    </Link>
+  );
+};
 
-export default FilterResult
+export default FilterResult;
