fe911a5c338595d07d3460e613f1f2606d544e62
Mayank Jain <mayankjainx404@gmail.com>
2023-03-19 21:19:31 +0530

    Added use State

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Landing from "./Pages/Landing";
 import { useDispatch } from "react-redux";
 import { updateRestaurant } from "./actions";
