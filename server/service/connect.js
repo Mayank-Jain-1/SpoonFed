89616a34a51c2a82222402cf432a016011361459
Mayank Jain <mayankjainx404@gmail.com>
2023-03-24 02:46:09 +0530

    Moved the mongodb password to environtment

--- server/service/connect.js
+++ server/service/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const uri =
-  "mongodb+srv://Mayank-Jain:aTppWPRq4ryMgQFy@spoon-fed.uoe2ztt.mongodb.net/spoon-fed?retryWrites=true&w=majority";
+const uri = `mongodb+srv://Mayank-Jain:${process.env.MONGO_PASSWORD}@spoon-fed.uoe2ztt.mongodb.net/spoon-fed?retryWrites=true&w=majority`;
 mongoose.set("strictQuery", false);
 
 const connectDB = async () => {
